edb21593bf31e06277c2e60dda156f2b5d7c3d73
aespin31 <aespin31@uic.edu>
2019-10-22 19:56:06 -0500

    hw3.c

---
+++ hw3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+	char *argsarray[] = {"/bin/ls", (char *)0};	
+
+	char command[10];
+	int end = 1;
+	int pid_number;
+	int exitstatus;
+	do{
+		printf("CS361 > ");
+		scanf("%s",command);
+		if(strcmp(command,"ls")==0){
+			int pid = fork();
+
+			if(pid ==0){
+				pid_number = getpid();
+				execv(argsarray[0], argsarray);
+			}
+			else{
+				int status;
+				wait(&status);
+				exitstatus=WEXITSTATUS(status);
+			}
+			
+	}
+		printf("pid:%d status:%d\n", pid_number, exitstatus);
+		end =0;
+	
+		
+		
+	}while(end == 1);
+
+	return 0;
+}
